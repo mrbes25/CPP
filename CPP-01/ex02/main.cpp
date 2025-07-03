1d74f292a9ff895e9ddabca0dad3894e10a2bf26
mrbes25 <bastian123schmid@gmail.com>
2025-07-03 12:06:17 +0200

    finished ex02

---
+++ CPP-01/ex02/main.cpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bschmid <bschmid@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/03 12:04:08 by bschmid           #+#    #+#             */
+/*   Updated: 2025/07/03 12:05:57 by bschmid          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+
+int main() {
+	// create string variable
+	std::string str = "HI THIS IS BRIAN";
+
+	// create a pointer to the string
+	std::string* stringPTR = &str;
+
+	// create a reference to the string
+	std::string& stringREF = str;
+
+	std::cout << "=== MEMORY ADRESSES ===" << std::endl;
+	std::cout << "Address of str:        " << &str << std::endl;
+    std::cout << "Address stored in PTR: " << stringPTR << std::endl;
+    std::cout << "Address of REF:        " << &stringREF << std::endl;
+
+    std::cout << "\n=== VALUES ===" << std::endl;
+    std::cout << "Value of str:          " << str << std::endl;
+    std::cout << "Value pointed by PTR:  " << *stringPTR << std::endl;
+    std::cout << "Value of REF:          " << stringREF << std::endl;
+
+    return 0;
+}
+
+
+// References are not pointers - they're ALIASES
+// References cannot be reassigned (they always refer to the same object)
+// Pointers can be reassigned to point to different objects
+// Both provide access to the same memory location
