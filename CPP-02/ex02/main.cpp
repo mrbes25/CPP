4f6daf23fce7fbc31e3315ed43c1abd3b151c369
mrbes25 <bastian123schmid@gmail.com>
2025-07-07 15:06:14 +0200

    cleanup

--- CPP-02/ex02/main.cpp
+++ CPP-02/ex02/main.cpp
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: bschmid <bschmid@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/07 14:48:27 by bschmid           #+#    #+#             */
-/*   Updated: 2025/07/07 14:49:42 by bschmid          ###   ########.fr       */
+/*   Created: 2025/07/07 15:02:24 by bschmid           #+#    #+#             */
+/*   Updated: 2025/07/07 15:05:15 by bschmid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,20 @@ int main(void) {
 	Fixed a;                                // Create a Fixed object with value 0 (default constructor)
 	Fixed const b(Fixed(5.05f) * Fixed(2)); // Create two temporary Fixed objects, multiply them, store result in b
 
+
+	std::cout << "\n=== SUBJECT MAIN ===" << std::endl;
+
 	std::cout << a << std::endl;    // Prints "0"
 	std::cout << ++a << std::endl;  // Increments a by 1/256, then prints (about "0.00390625")
 	std::cout << a << std::endl;    // Prints the same value (about "0.00390625")
 	std::cout << a++ << std::endl;  // Prints current value, then increments (prints "0.00390625")
 	std::cout << a << std::endl;    // Prints new value after increment (about "0.0078125")
-
 	std::cout << b << std::endl;    // Prints the value of b (about "10.1")
 	std::cout << Fixed::max(a, b) << std::endl; // Prints the maximum of a and b (which is b, "10.1")
-    // Additional tests for our new operators
-    Fixed c(5);    // Create Fixed object with value 5 using int constructor
+
+	std::cout << "\n=== EXTRA TESTS ===" << std::endl;
+
+	Fixed c(5);    // Create Fixed object with value 5 using int constructor
 	Fixed d(2);    // Create Fixed object with value 2 using int constructor
 
     std::cout << "c + d = " << c + d << std::endl; // Prints "c + d = 7"
